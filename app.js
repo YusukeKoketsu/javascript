576440f6cfad7e747ee212c171e58e8cf7acd59f
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-07-01 10:15:21 +0900

    oractise javascript

---
+++ app.js
@@ -0,0 +1,28 @@
+// let hello = 'Hello World';
+// alert(hello);
+
+// 変数の書き方
+// let int1 = 1;
+// let int2 = -10;
+// let floot1 = 3.14;
+// let str = 'JavaScriptを学ぼう'
+
+// alert(int1)
+// alert(int2)
+// alert(floot1)
+// alert(str)
+
+// 四則演算
+// alert(4+3);
+// alert(8-5);
+// alert(2*6);
+// alert(10/2);
+
+// 文字列の結合
+// alert('Hello'+'World')
+
+// 変数を利用する場合
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// let int1 = 3.14;
+// alert(str1+str2);
